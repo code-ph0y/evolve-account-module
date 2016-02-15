d895f9e80ee398bbe7300b7c69be58c4838c11f3
Ross Moroney <code.ph0y@googlemail.com>
2016-02-15 02:18:08 +0000

    Remove Email Config Details

--- resources/config/config.php
+++ resources/config/config.php
@@ -1,18 +1,4 @@
 <?php
 $config = array();
 
-$config['emailConfig'] = array(
-    'firstname' => 'My',
-    'lastname'  => 'Application',
-    'email'     => 'noreply@myapplication.com'
-);
-
-$config['signupEmail'] = array(
-    'subject' => 'Activate your new account'
-);
-
-$config['forgotEmail'] = array(
-    'subject' => 'Request your new password'
-);
-
 return $config;
